11f1d34fe41e323763af401c7e260642e41fefad
Nguyễn Ngọc Anh <anhnnst@yahoo.com>
2014-11-01 16:24:17 +0700

    Cai dat

---
+++ gen/com/example/mathlib/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.mathlib;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/mathlib/MathLib.java
@@ -0,0 +1,13 @@
+package com.example.mathlib;
+
+public class MathLib {
+
+	public MathLib() {
+		// TODO Auto-generated constructor stub
+	}
+	//cong 2 so a va b
+	public int add(int a, int b){
+		return a + b; //tra ve gia tri
+	}
+ 
+}
